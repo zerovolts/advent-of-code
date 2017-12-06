ebdb70962476c7ba40f2fc3e9177006ad762c80a
Zach Stone <hexaphixylix@yahoo.com>
2017-12-06 03:51:08 -0500

    add day-2 part-2

--- src/Day2/Solution.elm
+++ src/Day2/Solution.elm
@@ -3,7 +3,7 @@ module Day2.Solution exposing (part1, part2)
 import Day2.Data exposing (data)
 
 part1 = solve1 data
-part2 = 0
+part2 = solve2 data
 
 convert : String -> List (List Int)
 convert str = str
@@ -23,6 +23,32 @@ solve1 str =
   in
     List.sum diffs
 
+divisible : (Int, Int) -> Bool
+divisible (x, y) =
+  rem x y == 0
+
+makePairs : List a -> List (a, a)
+makePairs list =
+  List.concatMap
+    (\x ->
+      List.map
+        (\y -> (x, y))
+        list
+      |> List.filter (\(x, y) -> x /= y))
+    list
+
+findDivisible : List Int -> (Int, Int)
+findDivisible list =
+  List.filter (\pair -> divisible pair) (makePairs list)
+    |> List.head
+    |> Maybe.withDefault (1, 1)
+
 solve2 : String -> Int
 solve2 str =
-  0
+  let
+    list = convert str
+    dividePair = (\(x, y) -> x // y)
+  in
+    list
+      |> List.map (findDivisible >> dividePair)
+      |> List.sum
